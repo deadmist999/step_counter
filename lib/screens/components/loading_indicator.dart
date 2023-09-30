0f604f0d03d157aef8043db1082ef4eb4fad44f7
deadmist666 <kosenkovladysl4v@gmail.com>
2023-09-30 15:48:08 +0300

    - created loading indicator widget

---
+++ lib/screens/components/loading_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: CircularProgressIndicator(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
