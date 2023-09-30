d12756ef7a0299c66a7366ddd004f7df6f4ec179
deadmist666 <kosenkovladysl4v@gmail.com>
2023-09-30 16:05:01 +0300

    - added initial screen with implemented auth logic

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:step_counter/blocs/auth_bloc/auth_bloc.dart';
+import 'package:step_counter/screens/auth_screen/login_screen/login_screen.dart';
+import 'package:step_counter/screens/home_screen/home_screen.dart';
 import 'package:step_counter/services/auth_service.dart';
 
 import 'firebase_options.dart';
@@ -25,6 +27,14 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
+        ),
+        home: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if(state is AuthorizedUserState) {
+              return const HomeScreen();
+            }
+            return const LoginScreen();
+          },
         ),
       ),
     );
