be19117721556ea5f2701b739d9a36f1d2dc8cae
deadmist666 <kosenkovladysl4v@gmail.com>
2023-10-01 13:35:21 +0300

    - created home screen
    - features: sign out button in appbar, displaying step counter, achievement widget with navigation to achievement screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:step_counter/blocs/auth_bloc/auth_bloc.dart';
+import 'package:step_counter/blocs/pedometer_cubit/pedometer_cubit.dart';
+import 'package:step_counter/blocs/pedometer_cubit/pedometer_state.dart';
+import 'package:step_counter/screens/home_screen/components/achievement_widget.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => context.read<AuthBloc>().add(LogoutEvent()),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          BlocBuilder<PedometerCubit, PedometerState>(
+            builder: (context, state) {
+              return Text(state.pedometerStepCount);
+            },
+          ),
+          const AchievementWidget(),
+        ],
+      ),
     );
   }
 }
